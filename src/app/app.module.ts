095bab310565c0f5789edbf71769d99150eb98ea
Theo <theotournn@gmail.com>
2022-05-11 16:02:55 -0300

    Comps Imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './comps/main-header/main-header.component';
+import { CarouselComponent } from './comps/carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHeaderComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
