08fd13a44fb98b2ae097f6ae2018832e7309df1a
Theo <theotournn@gmail.com>
2023-09-05 01:48:13 -0300

    dropdown menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SobreComponent } from './comps/sobre/sobre.component';
+import { StreamingsComponent } from './comps/streamings/streamings.component';
+import { IntegrantesComponent } from './comps/integrantes/integrantes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sobre', component: SobreComponent },
+  { path: 'streaming', component: StreamingsComponent },
+  { path: 'integrantes', component: IntegrantesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/comps/main-header/main-header.component.ts
+++ src/app/comps/main-header/main-header.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ScrollService } from 'src/app/services/scroll.service';
 
 @Component({
   selector: 'app-main-header',
   templateUrl: './main-header.component.html',
 })
-export class MainHeaderComponent implements OnInit {
 
-  constructor() { }
+export class MainHeaderComponent {
+  constructor(private scrollService: ScrollService) { }
 
-  ngOnInit(): void {
+  scrollToSection(sectionId: string): void {
+    this.scrollService.scrollToSection(sectionId);
   }
-
 }
